79085dfe2208ab94820b9c7f92d1be1bb3ca2405
Seth Berrier <berriers@uwstout.edu>
2020-09-24 09:20:02 -0500

    Revert to original hello world

--- src/main.js
+++ src/main.js
@@ -1,22 +1,56 @@
-// Import the entire 'phaser' namespace
+// Bring in the phaser library
 import Phaser from 'phaser'
 
-import GameScene from './scenes/GameScene'
-
-// Core phaser configuration object
 const config = {
+  // Configure Phaser graphics settings
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+
+  // Configure physics settings
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 400 }
+      gravity: { y: 200 }
     }
+  },
+
+  // Establish scene callbacks
+  scene: {
+    preload: preload,
+    create: create
   }
 }
 
-// Start Phaser by making a Phaser.Game object and passing in the
-// config object from above.
+// Initialize the base phaser game object (must always be done once)
 const game = new Phaser.Game(config)
-game.scene.add('main', GameScene, true)
+
+// pre-load scene callback (loads all assets)
+function preload () {
+  this.load.image('sky', 'assets/skies/space3.png')
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+  this.load.image('red', 'assets/particles/red.png')
+}
+
+// Create scene callback: layout and setup the scene
+function create () {
+  // Add background image
+  this.add.image(400, 300, 'sky')
+
+  // Create and configure a particle emitter
+  const particles = this.add.particles('red')
+  const emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD'
+  })
+
+  // Create and animate the logo
+  const logo = this.physics.add.image(400, 100, 'logo')
+  logo.setVelocity(100, 200)
+  logo.setBounce(1, 1)
+  logo.setCollideWorldBounds(true)
+
+  // Make the particle emitter follow the logo
+  emitter.startFollow(logo)
+}
