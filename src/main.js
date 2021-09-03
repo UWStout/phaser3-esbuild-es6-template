4e46e0953730efd066e2dc2cf7bc9113b44a9591
Seth Berrier <berriers@uwstout.edu>
2021-09-03 13:29:14 -0500

    End of Tutorial 1 - Modularized our scene

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // Bring in the phaser library
 import Phaser from 'phaser'
 
+// Bringing in our base example scene
+import ExampleScene from './scenes/Example.js'
+
 const config = {
   // Configure Phaser graphics settings
   type: Phaser.AUTO,
@@ -13,45 +16,11 @@ const config = {
     arcade: {
       gravity: { y: 200 }
     }
-  },
-
-  // Establish scene callbacks
-  scene: {
-    preload: preload,
-    create: create
   }
 }
 
 // Initialize the base phaser game object (must always be done once)
-// eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config)
 
-// pre-load scene callback (loads all assets)
-function preload () {
-  this.load.image('sky', 'assets/skies/space3.png')
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-  this.load.image('red', 'assets/particles/red.png')
-}
-
-// Create scene callback: layout and setup the scene
-function create () {
-  // Add background image
-  this.add.image(400, 300, 'sky')
-
-  // Create and configure a particle emitter
-  const particles = this.add.particles('red')
-  const emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD'
-  })
-
-  // Create and animate the logo
-  const logo = this.physics.add.image(400, 100, 'logo')
-  logo.setVelocity(100, 200)
-  logo.setBounce(1, 1)
-  logo.setCollideWorldBounds(true)
-
-  // Make the particle emitter follow the logo
-  emitter.startFollow(logo)
-}
+// Add and auto-starting ExampleScene
+game.scene.add('ExampleScene', ExampleScene, true)
